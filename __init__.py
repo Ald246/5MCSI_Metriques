0a378c12749231e091e5106ebb948369c4ef0a9e
Ald246 <aldjiarahem49@gmail.com>
2025-12-03 11:14:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,8 +5,7 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+app = Flask(_name_)                                                                                                                  
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #Comm                                                                                                                               
@@ -24,3 +23,11 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
+if _name_ == "_main_":
+  app.run(debug=True)
