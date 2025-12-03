077d3fd2f7a5b6f3601458c11c2ac97d3ff55354
Ald246 <aldjiarahem49@gmail.com>
2025-12-03 12:11:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,29 +5,11 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(_name_)                                                                                                                  
+app = Flask(__name__)                                                                                                                  
+                                                                                                                                       
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #Comm                                                                                                                               
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-@app.route('/tawarano/')
-def meteo():
-    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")
-@app.route("/histogramme/")
-def histogramme():
-    return render_template("histogramme.html")
-if _name_ == "_main_":
+    return render_template('hello.html') #Com
+  
+if __name__ == "__main__":
   app.run(debug=True)
